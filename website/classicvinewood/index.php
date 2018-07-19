22856406204cfd18453ec7e749b6ca263c48aeda
LVDDR <barpor90@gmail.com>
2018-07-19 17:27:29 +0200

    First page of clasicvinewood

---
+++ website/classicvinewood/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Classicvinewood</title>
+    <link rel="stylesheet" type="text/css" href="/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+    <div class="maindiv">
+        <img class="logo" src="img/cv_logo.png" alt="">
+        <h2>Everythin looks good through the lens <br> of nostalgia, even the 1980s.</h2>
+        <div class="info">
+            <img src="img/roll.png" alt="">
+            <div class="left">
+                <h3>Sequels, remakes, romantic comedy sequels, superheroes, CGI nonsense <br> and one-dimensional androgynous vampires. <br> That's the Vinewood business model these days</h3>
+                <p>Low-risk, high-concept blockbusters supported by massive merchandising and completly stripped of all nuance and subtext so that Eastern Europeans and Asians don;t get confused. Unable to tell a succinct stroy in 90 minutes, directors make 3 hour films that frag on like someone shot the editor. The world has gone digital, which means no more tough decisions about brevity. The film industry is dead and, to add insult to injury, we have to watch as gutless studio execs strip the last pieces of meat from its decaying corpse.</p>
+                <h3>But it wasn't always this way...</h3>
+                <p>Remember a time when America still knew how to build things and win wars? A time before political correctness, when you could depict minorities any way you wanted? A time before CGI and special effects when, if you needed a monster, you made one yourself out of seaweed and egg boxes and fear? A time when you could smoke int the cinema and the commercials before movies were for folding bicycles and local restaurants? A time when leading women fainted at a man's slightest touch and leading men were leathery, strong-jawed-lotharios who died in their forties from liver failure?</p>
+                <p>We do. Join us on a trip down memory lane, Everything looks good through the lens of nostalgia, even the 1980s.</p>
+                <h3>Here are some Vinewood classics not to be missed.</h3>
+            </div>
+            <div class="right">
+                <a href="" title="">
+            		<img src="img/bullwhip.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/highschool.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/rumrunner.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/manywives.jpg" alt="">
+            	</a>
+                <a href="" title="">
+                    <p>Click here or any movie to take a trip down Vinewood memory lane.</p>
+                </a>
+                <a href="" title="">
+            		<img src="img/americandivorce.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/vinewoodzombie.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/shoulderoforion.jpg" alt="">
+            	</a>
+                <a href="" title="">
+            		<img src="img/sanitorium.jpg" alt="">
+            	</a>
+                <img style="width: 100%; height:30px;" src="img/rollshort.png" alt="">
+            </div>
+            <img style="margin-bottom: -5px;" src="img/roll.png" alt="">
+        </div>
+        <div class="classic">
+            <h3>Featured classic...</h3>
+            <div class="left">
+                <img src="img/curseoftriton.jpg" alt="">
+                <p class="fact">Classic Fact
+                    <br> That was the first leading role for baseball star Ace Jones! </p>
+            </div>
+            <div class="right">
+                <div class="title">
+                    <p>Curse of Triton (1941)</p>
+                    <p>A Sam Austin Production</p>
+                </div>
+                <p class="about">This pirate film is noted for its exceptional gun battle scene in which live ammunition was mistakenly used. The propmaster went to prison, but not before the move won scores of awards for some amazing acting that wasn't really acting. Despite its heavy-handed attempts at morality lessons and a rewriting of Greek mythology (entirely lost on 99% of its audience), Curse of Triton remains a wildly-successful swashbuckling classic. It also introduced the first leading role for gbaseball star Ace Jones as legendary buccaneer Willie La Roche. An Irishman with a flair for French fashion, Willie pouts, winks, loots and plunders his way across the Bay of Biscay in lavish silk clothing and takes wenches in his signature way that eventually coined the term "Angry Pirate". In a tournament of Wayne Thunder. The finale includes the King of Spain, a harem, and a giant squid in a sensational sea battle that was filmed in Austin's pool.</p>
+            </div>
+            <img style="width: 100%; margin-top: 20px;" src="img/roll.png" alt="">
+            <img style="display: block; margin: 0 auto; margin-top: 20px;" src="img/leonoraad.jpg" alt="">
+        </div>
+    </div>
+</body>
+
+</html>

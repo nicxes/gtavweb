2a882773557a56ba51c10eba4c1275aa255e68bd
Bartek Portka <barpor90@gmail.com>
2018-07-22 16:32:20 +0200

    huj

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,8 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 
-gulp.task('images', function(){
-  return gulp.src('img/**/*.+(png|jpg|gif|svg)')
-  .pipe(imagemin())
-  .pipe(gulp.dest('website/classicvinewood/img/covers/'))
-});
-
-gulp.task('images', function(){
-  return gulp.src('**/*.+(png|jpg|gif|svg)')
-  .pipe(imagemin())
-  .pipe(gulp.dest('website/classicvinewood/img/covers/'))
+gulp.task('images', function() {
+    return gulp.src('**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('website/classicvinewood/img/covers/'))
 });
